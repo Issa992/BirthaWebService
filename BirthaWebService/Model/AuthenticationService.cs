19e64d37efc46e0bc6b1525171db628bef931a86
Benjamin Agic <benj769b@edu.easj.dk>
2018-11-21 12:05:00 +0100

    Added security controller for better login

---
+++ BirthaWebService/Model/AuthenticationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BirthaWebService.Controllers;
+
+namespace BirthaWebService.Model
+{
+   
+        public static class AuthenticationService
+        {
+            public static async Task<User> AuthenticateUser(User userToAuthenticate)
+            {
+                UserController userController = new UserController();
+                var UserList = userController.Get();
+
+                var user = await Task.Run(() => UserList.SingleOrDefault(s => s.Name == userToAuthenticate.Name && s.Password == userToAuthenticate.Password));
+
+                if (user == null)
+                {
+                    return null;
+                }
+
+                user.Password = null; 
+                return user;
+            }
+        }
+    
+}
