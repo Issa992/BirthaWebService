43090439465b579cab1da57e3f30e91f7afa24c2
Benjamin Agic <benj769b@edu.easj.dk>
2018-11-21 12:38:05 +0100

    Fixed

--- BirthaWebService/Controllers/HealthController.cs
+++ BirthaWebService/Controllers/HealthController.cs
@@ -41,7 +41,6 @@ namespace BirthaWebService.Controllers
                 }
             }
 
-
             return null;
         }
 
--- BirthaWebService/Startup.cs
+++ BirthaWebService/Startup.cs
@@ -24,6 +24,7 @@ namespace BirthaWebService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -33,6 +34,13 @@ namespace BirthaWebService
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCors(
+                    options =>
+                    {
+                        options.AllowAnyOrigin().AllowAnyMethod();
+                        // allow everything from anywhere
+                    });
+
             }
 
             app.UseMvc();
