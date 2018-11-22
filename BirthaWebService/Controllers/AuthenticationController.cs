1eac7e60aff681923da5dbe01c7fb80639334f79
Benjamin Agic <benj769b@edu.easj.dk>
2018-11-22 12:43:10 +0100

    Added User Identity

---
+++ BirthaWebService/Controllers/AuthenticationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BirthaWebService.Model;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthaWebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+
+        // POST: api/Login
+        [HttpPost]
+        public async Task<ActionResult> Authenticate([FromBody] User userToAuthenticate)
+        {
+            var user = await IdentityUser.AuthenticateUser(userToAuthenticate);
+
+            if (user == null)
+                
+                return Unauthorized();
+
+            return Ok(user);
+        }
+
+
+    }
+}
---
+++ BirthaWebService/Model/IdentityUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BirthaWebService.Controllers;
+
+namespace BirthaWebService.Model
+{
+    public static class IdentityUser
+    {
+       
+            public static async Task<User> AuthenticateUser(User userToAuthenticate)
+            {
+                UserController uController = new UserController();
+                var userList = uController.Get();
+
+                var user = await Task.Run(() => userList.SingleOrDefault(x => x.Name == userToAuthenticate.Name && x.Password == userToAuthenticate.Password));
+
+                if (user == null)
+                {
+                    return null;
+                }
+
+                user.Password = null; 
+                return user;
+            }
+        
+    }
+}
